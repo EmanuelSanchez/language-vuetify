c476bc1447ac0f09df1b0dca3f93368eb59d4f75
Emanuel84 <emanuelsab@usb.ve>
2018-02-17 19:36:41 -0400

    cleaning the repo

--- spec/language-vuetify-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageVuetifyView from '../lib/language-vuetify-view';
-
-describe('LanguageVuetifyView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
