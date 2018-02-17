3d84067d3584fd6990f398e5aee77f6a9699dc91
Emanuel84 <emanuelsab@usb.ve>
2018-02-17 18:59:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vuetify component support in Atom
+
+Adds snippets of Vuetify to Vue component files in Atom.
---
+++ spec/language-vuetify-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageVuetifyView from '../lib/language-vuetify-view';
+
+describe('LanguageVuetifyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
